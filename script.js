dc94f626d61a702af0d5e2c6dc99d5bdb6f51207
Bhargav Penta <150350442+bhargavsunny912@users.noreply.github.com>
2025-10-28 12:42:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+let s1=document.getElementById("square1");
+let s2=document.getElementById("square2");
+let s3=document.getElementById("square3");
+
+s1.addEventListener('mouseover',()=>{
+	s2.style.backgroundColor='#6F4E37';
+	s3.style.backgroundColor='#6F4E37';
+});
+s1.addEventListener('mouseout',()=>{
+	s2.style.backgroundColor='#E6E6FA';
+	s3.style.backgroundColor='#E6E6FA';
+});
+
+s2.addEventListener('mouseover',()=>{
+	s1.style.backgroundColor='#6F4E37';
+	s3.style.backgroundColor='#6F4E37';
+});
+s2.addEventListener('mouseout',()=>{
+	s3.style.backgroundColor='#E6E6FA';
+	s1.style.backgroundColor='#E6E6FA';
+});
+
+s3.addEventListener('mouseover',()=>{
+	s2.style.backgroundColor='#6F4E37';
+	s1.style.backgroundColor='#6F4E37';
+});
+s3.addEventListener('mouseout',()=>{
+	s2.style.backgroundColor='#E6E6FA';
+	s1.style.backgroundColor='#E6E6FA';
+});
